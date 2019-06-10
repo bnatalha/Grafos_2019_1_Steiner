18c9b5504f78851faa174fc3d73a172f2033834f
Wilson Farias <wilsonsf1@gmail.com>
2019-06-10 20:29:35 -0300

    Escreve matriz de adjecência que representa o grafo em arquivo

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <vector>
@@ -6,6 +7,7 @@
 
 using namespace std;
 
+void writeFile(const vector<vector<int>> &);
 /**
  * Classe principal para a inicialização e execução do programa
  */
@@ -20,13 +22,15 @@ int main(int argc, char const *argv[])
 
     int totalVertices = steinerCount + terminalCount + 1;
 
-    vector< vector<int> > matrix (totalVertices);
-    for(int i = 0; i < totalVertices; i++){
+    vector<vector<int>> matrix(totalVertices);
+    for (int i = 0; i < totalVertices; i++)
+    {
         matrix[i].resize(totalVertices, -1);
         matrix[i][i] = 0;
     }
-    
-    for (int i = 0; i < edgesCount; i++){
+
+    for (int i = 0; i < edgesCount; i++)
+    {
         int o, d, c;
         std::cin >> o >> d >> c;
 
@@ -35,8 +39,6 @@ int main(int argc, char const *argv[])
         steiner.insert(o);
         terminal.insert(d);
 
-        
-
         Edge newEdge(o, d, c);
 
         edges.push_back(newEdge);
@@ -44,31 +46,48 @@ int main(int argc, char const *argv[])
 
     std::cout << std::endl;
     std::cout << "Steiner: ";
-    for (int s : steiner){
+    for (int s : steiner)
+    {
         std::cout << s << " ";
     }
     std::cout << std::endl
               << std::endl;
-    
+
     std::cout << "Terminal: ";
-    for (int t : terminal){
+    for (int t : terminal)
+    {
         std::cout << t << " ";
     }
     std::cout << std::endl
               << std::endl;
 
-    for (Edge e : edges){
+    for (Edge e : edges)
+    {
         std::cout << e << std::endl;
     }
 
     std::cout << std::endl;
 
-    for (int i = 0; i < totalVertices; i++){
-        for (int j = 0; j < totalVertices; j++){
-            std::cout << setfill(' ') << setw(2) << matrix[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
+    writeFile(matrix);
 
     return 0;
+}
+
+void writeFile(const vector<vector<int>> &matrix)
+{
+    ofstream example("out/output.txt");
+    if (example.is_open())
+    {
+        for (unsigned long i = 0; i < matrix.size(); i++)
+        {
+            for (unsigned long j = 0; j < matrix[i].size(); j++)
+            {
+                std::cout << setfill(' ') << setw(2) << matrix[i][j] << " ";
+                example << setfill(' ') << setw(2) << matrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+            example << std::endl;
+        }
+        example.close();
+    }
 }
